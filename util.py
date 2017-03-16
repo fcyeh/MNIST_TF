1cb29980b92a8d68090b19399bbd489b3e031467
Ming-Yuan Yu <myyu@umich.edu>
2017-03-16 01:58:22 -0400

    clean more code

--- util.py
+++ util.py
@@ -3,17 +3,16 @@ import tensorflow as tf
 def build_model(x, y, training=True, reuse=None, num_filters=16, ksize=4, stride=2):
     with tf.variable_scope('model'):
         x = tf.reshape(x, [-1, 28, 28, 1])
-        with tf.variable_scope('conv1'):
-            x = tf.layers.conv2d(x, num_filters, ksize, stride,
-                                use_bias=False, padding='same',
-                                reuse=reuse, name='conv2d')
-            x = tf.layers.batch_normalization(x,
-                                scale=False, training=training,
-                                reuse=reuse, name='bn')
-            x = tf.nn.relu(x, name='relu')
+        x = tf.layers.conv2d(x, num_filters, ksize, stride,
+                            use_bias=False, padding='same',
+                            reuse=reuse, name='conv1')
+        x = tf.layers.batch_normalization(x,
+                            scale=False, training=training,
+                            reuse=reuse, name='bn1')
+        x = tf.nn.relu(x, name='relu1')
 
         x = tf.layers.conv2d(x, num_filters, ksize, stride, padding='same',
-                                reuse=reuse, name='conv2')
+                            reuse=reuse, name='conv2')
 
         logits = tf.layers.conv2d(x, 10, x.shape[1:3].as_list(), reuse=reuse, name='logits')
         y = tf.reshape(y, [-1, 1, 1, 10], name='labels')
